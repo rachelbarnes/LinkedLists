36972a757b8037f7609271ef1a3140f549056901
rachelbarnes <rachelbarnesjn116@yahoo.com>
2016-10-26 22:47:32 -0400

    :

--- LinkedLists/Node.cs
+++ LinkedLists/Node.cs
@@ -39,6 +39,27 @@ namespace LinkedLists {
             }
             return value.ToString() + " -> " + next.ToString();
         }
+        public string ToStringWithoutIntervalCharacters() {
+            var result = "";
+            if (next == null) {
+                return value.ToString();
+            }
+            return value.ToString() + next.ToString();
+        }
+
+        //public int ToInt() {
+        //    var result = "";
+        //    var reversedResult = "";
+        //    if (next == null) {
+        //        result = value.ToString();
+        //    }
+        //    result = value.ToString() + next.ToString();
+        //    foreach (var number in result) {
+        //        reversedResult += "";
+        //    }
+        //    return int.Parse(reversedResult);
+        //}
+
         //static bool Compare<T> (T x, T y) where T : class {
         //    return x == y; 
         //}
@@ -70,7 +91,7 @@ namespace LinkedLists {
         public DoubleNode() { }
 
         public DoubleNode(T _value) {
-            value = _value; 
+            value = _value;
         }
         public int Count() {
             if (next != null) {
@@ -79,15 +100,66 @@ namespace LinkedLists {
             return 1;
         }
         public override string ToString() {
-            if (next == null) { 
-                return value.ToString(); 
+            if (next == null) {
+                return value.ToString();
             }
-            return value.ToString() + " <-> " + next.ToString(); 
+            return value.ToString() + " <-> " + next.ToString();
+        }
+        public override bool Equals(Object obj) {
+            if (obj == null || GetType() != obj.GetType()) {
+                return false;
+            }
+            DoubleNode<T> p = (DoubleNode<T>)obj;
+            return p.ToString() == this.ToString();
         }
     }
 
+
     [TestFixture]
     public class NodeTesting {
+        [Test]
+        public void ToIntTesting() {
+            var myNumber = new Node<int>(8, new Node<int>(4, new Node<int>(5, new Node<int>(3, new Node<int>(2, new Node<int>(9, null))))));
+            var expected = "923548";
+            var actual = myNumber.ToStringWithoutIntervalCharacters();
+            Assert.AreEqual(expected, actual);
+        }
+        public DoubleNode<char> sampleDoubleLinkedNode() {
+            var a = new DoubleNode<char>('a');
+            var b = new DoubleNode<char>('b');
+            var c = new DoubleNode<char>('c');
+            var d = new DoubleNode<char>('d');
+            a.previous = null;
+            a.next = b;
+            b.previous = a;
+            b.next = c;
+            c.previous = b;
+            c.next = d;
+            d.previous = c;
+            d.next = null;
+            return a;
+        }
+        public DoubleNode<char> secondSampleDoubleLinkedNode() {
+            var a = new DoubleNode<char>('a');
+            var b = new DoubleNode<char>('b');
+            var c = new DoubleNode<char>('c');
+            var d = new DoubleNode<char>('d');
+            a.previous = null;
+            a.next = b;
+            b.previous = a;
+            b.next = c;
+            c.previous = b;
+            c.next = d;
+            d.previous = c;
+            d.next = null;
+            return a;
+        }
+        [Test]
+        public void EqualsTestDoubleNode() {
+            var expected = true;
+            var actual = secondSampleDoubleLinkedNode().Equals(sampleDoubleLinkedNode());
+            Assert.AreEqual(expected, actual);
+        }
         //these are tests for both Nodes and DoubleNodes (singly and doubly linked lists)
         [Test]
         public void CountMethodForNode() {
@@ -108,7 +180,7 @@ namespace LinkedLists {
             var a = new DoubleNode<char>('a');
             var b = new DoubleNode<char>('b');
             var c = new DoubleNode<char>('c');
-            var d = new DoubleNode<char>('d'); 
+            var d = new DoubleNode<char>('d');
             /*
             Because the only way to do declare a doubly linked list is to declare their links separate from the constructor, the DoubleNode<char>(T value) only takes the value...
                 There may be a way of making the links in constructors and objects, but no easy way for sure. It is going to be better to assign their links elsewhere (like below)
@@ -121,9 +193,9 @@ namespace LinkedLists {
             c.next = d;
             d.previous = c;
             d.next = null;
-            var expected = 4; 
+            var expected = 4;
             var actual = a.Count();
-            Assert.AreEqual(expected, actual); 
+            Assert.AreEqual(expected, actual);
         }
         [Test]
         public void ToStringForDoublyLinkedNodes() {
@@ -141,7 +213,7 @@ namespace LinkedLists {
             d.next = null;
             var expected = "a <-> b <-> c <-> d";
             var actual = a.ToString();
-            Assert.AreEqual(expected, actual); 
+            Assert.AreEqual(expected, actual);
         }
     }
 }
---
+++ LinkedLists/SumLists.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; 
+namespace LinkedLists {
+    /*
+    Two linked lists in reverse (when a linked list is written, it starts with the head, and adds the nodes to the left)
+        add the reverse of 2 int linked lists, then write it a linked list form, making that reversed as well:
+        for example: input: (817 + 549)
+            7 -> 1 -> 8 > ; 9 -> 4 -> 5; 
+                     output: (1366)
+    */ 
+    public class SumLists {
+    }
+}
