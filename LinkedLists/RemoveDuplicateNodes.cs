cb7a1cac982c6497a11e8394ce0f1b96246d0657
rachelbarnes <rachelbarnesjn116@yahoo.com>
2016-11-14 12:07:19 -0500

    Making the RemoveDuplicateNodes.cs pass

--- LinkedLists/RemoveDuplicateNodes.cs
+++ LinkedLists/RemoveDuplicateNodes.cs
@@ -16,22 +16,25 @@ namespace LinkedLists {
             }
             Cell<char> a = null; //note to self, this has to be null... 
             foreach (var item in list) {
-                //if (item == list.Last()) {
-                //    a.value = item;
-                //    a.next = null; 
-                //}
                 a = new Cell<char> {
                     value = item,
-                    next = a
-                }; 
-                 //look at line 32-36 in SumLists.cs... what's different that I get the linked list formed there, but not here?
-            } // now the reversing of hte linked list...
-                 //for future reference, having that if (item == list.last()) messes things up... it almost throws away the other 
-                 //nodes in the linked list, forgets them and only returns the last, hwich is why i was only getting 'd' for my value 
-                    //in my supposed to be 4 noded linked list but only having 1 node.
-            return a.PrintCellRec();
+                    next = a //this is a good point of recursion... it's actually setting up what's goin on in the tests... 
+                             //value is the value, next is null. if there's another item in the list, then the next item is assigned to another node,
+                             //and the link is null, and continues through the list of items in teh foreach loop, but when the foreach is done, there is no link, so a being delcared null in line 17 is > impt than it looks
+                };
+            }
+            Cell<char> b = null; // new Cell<char>();//  = null; 
+            while (a != null) {
+                b = new Cell<char> {
+                    value = a.value,
+                    next = b
+                };
+                a = a.next; 
+            }
+                return b.PrintCellRec();
         }
     }
+
     [TestFixture]
     public class RemoveDupsTests {
         [Test]
